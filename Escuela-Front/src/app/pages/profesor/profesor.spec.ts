ee79f6b6858cdaf70395f84ab0458a76976c0973
kevinhg04 <kevinhga04@gmail.com>
2025-10-19 20:45:42 -0600

    Update profesor.spec.ts

--- Escuela-Front/src/app/pages/profesor/profesor.spec.ts
+++ Escuela-Front/src/app/pages/profesor/profesor.spec.ts
@@ -1,23 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+// Importamos la clase con el nombre correcto: ProfesorComponent
+import { ProfesorComponent } from './profesor'; 
 
-import { Profesor } from './profesor';
+// Usamos el nombre completo del componente en el bloque de descripción
+describe('ProfesorComponent', () => { 
+  // La variable del componente debe ser de tipo ProfesorComponent
+  let component: ProfesorComponent; 
+  let fixture: ComponentFixture<ProfesorComponent>;
 
-describe('Profesor', () => {
-  let component: Profesor;
-  let fixture: ComponentFixture<Profesor>;
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      // El standalone component se importa con el nombre completo
+      imports: [ProfesorComponent] 
+    })
+    .compileComponents();
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [Profesor]
-    })
-    .compileComponents();
+    // Al crear el componente, usamos el nombre completo
+    fixture = TestBed.createComponent(ProfesorComponent); 
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-    fixture = TestBed.createComponent(Profesor);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 });
