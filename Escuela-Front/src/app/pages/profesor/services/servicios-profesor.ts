551bca93f735f5b2540a17df8cc2f1333d7e59bd
kevinhg04 <kevinhga04@gmail.com>
2025-11-03 00:01:01 -0600

    Update

--- Escuela-Front/src/app/pages/profesor/services/servicios-profesor.ts
+++ Escuela-Front/src/app/pages/profesor/services/servicios-profesor.ts
@@ -3,9 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { CalificacionRegistro, CalificacionResponse } from '../../../models/calificacion';
 import { Profesor } from '../../../models/Profesor';
-import { AsignacionDocente, MateriasCamposFormativos } from '../../../models/AsignaciónMateria';
-import { MateriaAsignada } from '../../../models/AsignaciónMateria';
-import { MateriaResponse } from '../../../models/AsignaciónMateria';
+import { MateriasCamposFormativos } from '../../../models/AsignaciónMateria';
 import { InscripcionDTO } from '../../../models/Materia';
 
 
@@ -17,42 +15,29 @@ export class ServiciosProfesor {
 
   constructor(private http: HttpClient) { }
 
-    /**
-     * Obtiene el perfil completo del usuario (profesor) por su ID.
-     * @param idUsuario El ID (UUID) del profesor logueado.
-     */
   obtenerPerfilUsuario(idUsuario: string): Observable<Profesor> {
     return this.http.get<Profesor>(`${this.apiUrlBase}/usuarios/BuscarUsuario/${idUsuario}`);
   }
-  /**
-   * Actualiza los datos del profesor por su ID.
-   * @param idProfesor El ID (UUID) del profesor a actualizar.
-   * @param profesor Los datos actualizados del profesor.
-   */
+
   editarProfesor(idProfesor: string, profesor:Profesor ): Observable<Profesor> {
     return this.http.put<Profesor>(`${this.apiUrlBase}/Profesores/profesor/${idProfesor}`, profesor);
   }
-  // ✅ Obtener asignación del docente
+
   obtenerAsignacionDocente(idProfesor: string): Observable<any> {
-    return this.http.get<any>(
-      `${this.apiUrlBase}/asignacion-docente/resumen-profesor/reciente?idProfesor=${idProfesor}`
+    return this.http.get<any>(`${this.apiUrlBase}/asignacion-docente/resumen-profesor/reciente?idProfesor=${idProfesor}`
     );
   }
 
-  // ✅ Obtener materias por grado
   obtenerMateriasPorGrado(idGrado: string): Observable<MateriasCamposFormativos[]> {
     const url = `${this.apiUrlBase}/asignacion/listar-por-grado?idGrado=${idGrado}`;
     return this.http.get<MateriasCamposFormativos[]>(url);
   }
   
-
-
    filtrarInscripciones(gradoId: string, grupoId: string, cicloId: string): Observable<InscripcionDTO[]> {
     const params = new HttpParams()
       .set('gradoId', gradoId)
       .set('grupoId', grupoId)
       .set('cicloId', cicloId);
-
     return this.http.get<InscripcionDTO[]>(`${this.apiUrlBase}/inscripcion/filtrar`, { params });
   }
 
@@ -61,7 +46,6 @@ export class ServiciosProfesor {
       .set('page', page.toString())
       .set('size', size.toString())
       .set('sortBy', sortBy);
-
     return this.http.get<CalificacionResponse>(`${this.apiUrlBase}/materias`, { params });
   }
 
